daa967f8658029df2176eb213df2320520d13bb2
Ralfi Salhon <ralfisalhon@gmail.com>
2020-03-13 16:52:46 -0400

    Added logo and one styles jsx

---
+++ src/styles.jsx
@@ -0,0 +1,17 @@
+const s = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: '#001255',
+    width: '100vw',
+    height: '100vh',
+    overflow: 'hidden',
+    padding: '2rem',
+  },
+  image: {
+    height: '10vw',
+    width: '10vw',
+  },
+};
+
+export default s;
