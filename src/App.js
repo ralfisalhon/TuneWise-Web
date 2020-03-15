121d91dc8e83e1a0ff07d3d03caa3fbe1174ad63
Ralfi Salhon <ralfisalhon@gmail.com>
2020-03-14 23:01:37 -0400

    Added Spotify Login

--- src/App.js
+++ src/App.js
@@ -5,6 +5,25 @@ import Clickable from './reusables/clickable';
 
 import logo from './assets/tunewise_logo.png';
 
+export const authEndpoint = 'https://accounts.spotify.com/authorize?';
+
+const clientId = '895eddbce28a406e92d83b3ca8451560';
+const redirectUri = 'http://localhost:3000';
+const scopes = ['user-read-currently-playing', 'user-read-playback-state'];
+
+const hash = window.location.hash
+  .substring(1)
+  .split('&')
+  .reduce(function(initial, item) {
+    if (item) {
+      var parts = item.split('=');
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+    }
+    return initial;
+  }, {});
+
+window.location.hash = '';
+
 function App() {
   return (
     <div className="wrapper">
@@ -17,6 +36,14 @@ function App() {
         <p className="text">_____ or _____</p>
         <div style={{ height: '10px' }} />
         <Clickable text={'join existing.'} onClick={() => alert('Join Existing')} />
+        <a
+          className="btn btn--loginApp-link"
+          href={`${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+            '%20'
+          )}&response_type=token&show_dialog=true`}
+        >
+          Login to Spotify
+        </a>
       </div>
     </div>
   );
