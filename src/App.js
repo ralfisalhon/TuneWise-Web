6add4d245079e1665c33807588b4c1021c5600f3
Ralfi Salhon <ralfisalhon@gmail.com>
2020-03-14 22:50:31 -0400

    Phone resizing complete

--- src/App.js
+++ src/App.js
@@ -8,13 +8,14 @@ import logo from './assets/tunewise_logo.png';
 function App() {
   return (
     <div className="wrapper">
-      <div className="inner">
+      <div className={window.innerWidth > 500 ? 'inner' : 'innerMobile'}>
         <p className="text">_____ get ur bop on _____</p>
         <div className="logoContainer">
           <img alt="logo" src={logo} className="image" />
         </div>
         <Clickable filled text={'create session.'} color="white" onClick={() => alert('Create Session')} />
         <p className="text">_____ or _____</p>
+        <div style={{ height: '10px' }} />
         <Clickable text={'join existing.'} onClick={() => alert('Join Existing')} />
       </div>
     </div>
